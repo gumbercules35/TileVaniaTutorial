76b91cd782df22cc27f6d79ea4d38d40cc8036fb
Gumbercules35 <mattwalden@gmail.com>
2023-05-18 16:14:02 +0100

    Enemy Prefab and basic movement logic added

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private Rigidbody2D enemyRigidBody;
+    private BoxCollider2D edgeDetector;
+
+    private void Awake() {
+        enemyRigidBody = gameObject.GetComponent<Rigidbody2D>();
+        edgeDetector = gameObject.GetComponent<BoxCollider2D>();
+    }
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        enemyRigidBody.velocity = new Vector2(moveSpeed,0f);
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        Debug.Log("Bump");
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    private void FlipEnemyFacing(){
+        gameObject.transform.localScale = new Vector2 (-(Mathf.Sign(enemyRigidBody.velocity.x)), 1f);
+    }
+}
